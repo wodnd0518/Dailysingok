a4f14506c2fe4fa9c0ba6e61511870a69131379b
Jungwoo Lee <jeong1135@gmail.com>
2015-12-06 17:53:18 +0900

    /history and /song/<song_id> are added. Templates handle them.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'true'
 db = SQLAlchemy(app)
@@ -34,6 +35,8 @@ class AdminView(AdminIndexView):
 
 
 class SongView(ModelView):
+    column_list = ('id', 'title', 'artist', 'created_at',)
+
     def __init__(self, session, **kwargs):
         super(SongView, self).__init__(Song, session, **kwargs)
 
@@ -44,7 +47,20 @@ admin.add_view(SongView(db.session, name='Song'))
 # View
 @app.route("/")
 def index():
+
     return render_template("index.html")
+
+
+@app.route("/history")
+def history():
+    songs = Song.query.all()
+    return render_template("history.html", songs=songs)
+
+
+@app.route("/song/<song_id>")
+def song(song_id):
+    song = Song.query.get_or_404(song_id)
+    return render_template("detail.html", song=song)
 
 
 if __name__ == "__main__":
