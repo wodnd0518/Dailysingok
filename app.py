e57b73b16f183ce4f7cea232b8a012b7c90c0a2d
unknown <>
2015-12-20 21:36:01 +0900

    잘못되서 원래되로 복귀

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import datetime
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super-secret'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'true'
 db = SQLAlchemy(app)
 
